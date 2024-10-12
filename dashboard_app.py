aae190e9ac0f944d6dee971d5228bd48950b4bad
Aportra <ap222179@umconnect.umt.edu>
2024-10-12 12:32:47 -0700

    adding data to repo to deploy app

--- dashboard_app.py
+++ dashboard_app.py
@@ -7,7 +7,7 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 from datetime import datetime
 
-data = pd.read_csv('C:\\Users\\aport\\OneDrive\\Documents\\School\\Fall Semester 2024\\Telling Stories w Data\\dashboard-assignment\\query-results.txt'
+data = pd.read_csv('query-results.txt'
                    ,sep = '\t', on_bad_lines = 'skip')
 data.loc[data['state'] == 'Sport Utility 4D']
 data.drop(12433,axis = 0,inplace= True)
